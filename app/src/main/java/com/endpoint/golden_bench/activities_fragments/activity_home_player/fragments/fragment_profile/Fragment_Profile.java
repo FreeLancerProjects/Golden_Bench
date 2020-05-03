02c29d4e93a92f40cc9da3f5f8f8d795a6cfefc8
End Point <ahmedmohamed23345@gmail.com>
2020-05-03 12:17:10 +0200

    ch

--- app/src/main/java/com/endpoint/golden_bench/activities_fragments/activity_home_player/fragments/fragment_profile/Fragment_Profile.java
+++ app/src/main/java/com/endpoint/golden_bench/activities_fragments/activity_home_player/fragments/fragment_profile/Fragment_Profile.java
@@ -1,6 +1,7 @@
 package com.endpoint.golden_bench.activities_fragments.activity_home_player.fragments.fragment_profile;
 
 import android.app.Dialog;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,14 +9,22 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.endpoint.golden_bench.R;
 import com.endpoint.golden_bench.activities_fragments.activity_home_player.HomeplayerActivity;
+import com.endpoint.golden_bench.adapter.Category_Adapter;
+import com.endpoint.golden_bench.adapter.Categorys_Adapter;
+import com.endpoint.golden_bench.adapter.Photo_Adapter;
+import com.endpoint.golden_bench.adapter.Photos_Adapter;
 import com.endpoint.golden_bench.adapter.ViewPagerTitleAdapter;
 
 import com.endpoint.golden_bench.databinding.FragmentProfilePlayerBinding;
+import com.endpoint.golden_bench.models.MarketCatogryModel;
 import com.endpoint.golden_bench.models.UserModel;
 import com.endpoint.golden_bench.preferences.Preferences;
 
@@ -34,6 +43,10 @@ public class Fragment_Profile extends Fragment {
 
     private Preferences preferences;
     private UserModel userModel;
+    private Photo_Adapter category_adapter;
+    private Photos_Adapter photos_adapter;
+
+    private List<MarketCatogryModel.Data> dataList;
 
     public static Fragment_Profile newInstance() {
         return new Fragment_Profile();
@@ -50,6 +63,7 @@ public class Fragment_Profile extends Fragment {
 
 
     private void initView() {
+        dataList=new ArrayList<>();
         activity = (HomeplayerActivity) getActivity();
         preferences = Preferences.getInstance();
         userModel = preferences.getUserData(activity);
@@ -58,7 +72,42 @@ public class Fragment_Profile extends Fragment {
 
 
 
+        category_adapter = new Photo_Adapter(dataList, activity);
+        photos_adapter = new Photos_Adapter(dataList, activity);
+
+        binding.recview1.setLayoutManager(new LinearLayoutManager(activity,RecyclerView.HORIZONTAL,false));
+        binding.recview1.setAdapter(category_adapter);
+        binding.recView.setLayoutManager(new LinearLayoutManager(activity,RecyclerView.HORIZONTAL,false));
+        binding.recView.setAdapter(photos_adapter);
+        binding.recView2.setLayoutManager(new LinearLayoutManager(activity,RecyclerView.HORIZONTAL,false));
+        binding.recView2.setAdapter(photos_adapter);
+        binding.recview3.setLayoutManager(new LinearLayoutManager(activity,RecyclerView.HORIZONTAL,false));
+        binding.recview3.setAdapter(photos_adapter);
+//        binding.recservice.setLayoutManager(new LinearLayoutManager(activity, RecyclerView.HORIZONTAL,false));
+//        binding.recservice.setAdapter(categorys_adapter);
+//        binding.progBar.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(activity, R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+//        binding.progBarservice.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(activity, R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+//
+//        binding.progBar.setVisibility(View.GONE);
+//        binding.progBarservice.setVisibility(View.GONE);
+        Adddata();
+
     }
+
+    private void Adddata() {
+        dataList.add(new MarketCatogryModel.Data());
+        dataList.add(new MarketCatogryModel.Data());
+        dataList.add(new MarketCatogryModel.Data());
+        dataList.add(new MarketCatogryModel.Data());
+        dataList.add(new MarketCatogryModel.Data());
+        dataList.add(new MarketCatogryModel.Data());
+        dataList.add(new MarketCatogryModel.Data());
+        dataList.add(new MarketCatogryModel.Data());
+        category_adapter.notifyDataSetChanged();
+        photos_adapter.notifyDataSetChanged();
+
+    }
+
 
 
 
---
+++ app/src/main/java/com/endpoint/golden_bench/adapter/Photos_Adapter.java
@@ -0,0 +1,117 @@
+package com.endpoint.golden_bench.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.endpoint.golden_bench.R;
+import com.endpoint.golden_bench.databinding.ImageRowBinding;
+import com.endpoint.golden_bench.databinding.MostActiveRowBinding;
+import com.endpoint.golden_bench.models.MarketCatogryModel;
+
+import java.util.List;
+import java.util.Locale;
+
+import io.paperdb.Paper;
+
+public class Photos_Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final int ITEM_MESSAGE_LEFT = 1;
+    private final int ITEM_MESSAGE_RIGHT = 2;
+    private List<MarketCatogryModel.Data> orderlist;
+    private Context context;
+    private LayoutInflater inflater;
+    private String lang;
+    public Photos_Adapter(List<MarketCatogryModel.Data> orderlist, Context context) {
+        this.orderlist = orderlist;
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+        Paper.init(context);
+        lang = Paper.book().read("lang", Locale.getDefault().getLanguage());
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+
+
+            MostActiveRowBinding binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.most_active_row, parent, false);
+            return new EventHolder(binding);
+
+
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+
+            EventHolder msgLeftHolder = (EventHolder) holder;
+
+
+
+
+        }
+/*
+if(i==position){
+    if(i!=0) {
+        if (((EventHolder) holder).binding.expandLayout.isExpanded()) {
+            ((EventHolder) holder).binding.tvTitle.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            ((EventHolder) holder).binding.recView.setLayoutParams(new FrameLayout.LayoutParams(0, 0));
+            ((EventHolder) holder).binding.expandLayout.collapse(true);
+            ((EventHolder) holder).binding.expandLayout.setVisibility(View.GONE);
+
+
+
+        }
+        else {
+
+          //  ((EventHolder) holder).binding.tvTitle.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            ((EventHolder) holder).binding.recView.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT));
+            ((EventHolder) holder).binding.expandLayout.setVisibility(View.VISIBLE);
+
+           ((EventHolder) holder).binding.expandLayout.expand(true);
+        }
+    }
+    else {
+        eventHolder.binding.tvTitle.setBackground(activity.getResources().getDrawable(R.drawable.linear_bg_green));
+
+        ((EventHolder) holder).binding.tvTitle.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        ((EventHolder) holder).binding.recView.setLayoutParams(new FrameLayout.LayoutParams(0, 0));
+
+    }
+}
+if(i!=position) {
+    eventHolder.binding.tvTitle.setBackground(activity.getResources().getDrawable(R.drawable.linear_bg_white));
+    ((EventHolder) holder).binding.tvTitle.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+    ((EventHolder) holder).binding.recView.setLayoutParams(new FrameLayout.LayoutParams(0, 0));
+    ((EventHolder) holder).binding.expandLayout.collapse(true);
+
+
+}*/
+
+
+
+    @Override
+    public int getItemCount() {
+        return orderlist.size();
+    }
+
+    public class EventHolder extends RecyclerView.ViewHolder {
+        public MostActiveRowBinding binding;
+
+        public EventHolder(@NonNull MostActiveRowBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+
+        }
+    }
+
+
+}
